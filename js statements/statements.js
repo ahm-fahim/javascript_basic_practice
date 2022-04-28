4c5ba35488f05aab2efbe85e18870c10fa022cfb
mdFahimMorshed <contactahmfahim@gmail.com>
2022-04-28 19:33:48 +0600

    variables

---
+++ js statements/statements.js
@@ -0,0 +1,5 @@
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+console.log(z)
---
+++ variables/variables.js
@@ -0,0 +1,42 @@
+const num1 = 5;
+const num2 = 10;
+let sum = num1 + num2;
+
+console.log(sum);
+
+let value;
+console.log(value); //undefined
+
+let person = "Fahim", carName = "Volvo", price = 2000;
+
+let person2 = "Morshed",
+carName2 ="Toyota",
+price2 = 1000;
+
+
+// arithmetic
+
+let x = 5 + 10;
+console.log(x);
+
+let y = "John" + " " + "Doe";
+console.log(y);
+
+let sum2 = 2 + 4 + "7";
+console.log(sum2); 
+
+// Dollar sign $
+
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 3;
+
+console.log($);
+
+// Underscore(_)
+let _lastName = "Fahim";
+let _X = 2;
+let _100 = 3;
+
+console.log(_lastName);
+console.log(_100);
